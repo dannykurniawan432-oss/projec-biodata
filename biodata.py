8a93d758c902b41564697d2e2d7f1ce3da0e458d
danny kurniawan <dannykurniawan432@gmail.com>
2025-09-26 10:38:45 +0700

    pertama kali mencoba

---
+++ biodata.py
@@ -0,0 +1,11 @@
+# Definisi variabel
+nama = "danny kurniawan"
+umur = 20
+hobi = "running"
+
+#print
+print(f"nama saya adalah {nama}.")
+print(f"saat ini saya berumur {umur} tahun.")
+print(f"hobi saya adalah {hobi}.")
+
+
